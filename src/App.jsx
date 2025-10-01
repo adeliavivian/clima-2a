6fc76021027834419ad06326307b95c4205d7777
adeliavivian <adeliaconscett@gmail.com>
2025-10-01 10:48:47 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="app-container">
+      <div className="container-wrapper">
+        <header>
+          <h1>Consulta de Clima</h1>
+          <p>Exemplo de consumno de API com React</p>
+        </header>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
